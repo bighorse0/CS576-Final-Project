9888dd0d564e3238bc59707bba8bcb569cf3e637
Ryu-From-Streets <112056227+Ryu-From-Streets@users.noreply.github.com>
2024-12-02 21:28:43 -0500

    Created scripts for handling bubbles
    Created scripts for handling bubble and math problem creation.
    Not yet implemented:
    integration with game logic

---
+++ Assets/Scripts/BubbleTrigger.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleTrigger : MonoBehaviour
+{
+    [SerializeField] private int hardness;
+    [SerializeField] private GameObject bubble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    string IntToOperator(int operator_num) {
+        string operation = "";
+
+        switch (operator_num) {
+            case 0:
+                operation = "+";
+                break;
+            case 1:
+                operation = "-";
+                break;
+            case 2:
+                operation = "*";
+                break;
+            case 3: 
+                operation = "/";
+                break;
+        }
+
+        return operation;
+    }
+
+    Dictionary<string, string> GenProbWithRangeAndOperator(int range, int operation) {
+        Dictionary<string, string> prob_and_sols = new Dictionary<string, string>();
+
+        int operand_one = Random.Range(0, range);
+        int operand_two = Random.Range(0, range);
+        int correct_answer = 0;
+        int incorrect_answer = 0;
+        string problem = "";
+
+        switch (operation) {
+            case 0:
+                // Addition
+                correct_answer = operand_one + operand_two;
+                while (incorrect_answer == correct_answer) {
+                    incorrect_answer = Random.Range(0, 2 * correct_answer);
+                }
+                
+
+                problem = operand_one.ToString() + " + " + operand_two.ToString() + " = ?";
+                break;
+            case 1:
+                // Subtraction
+                correct_answer = operand_one - operand_two;
+                if (correct_answer < 0) {
+                    while (incorrect_answer == correct_answer) {
+                        incorrect_answer = Random.Range(2 * correct_answer, -2 * correct_answer);
+                    }
+                }
+                else {
+                    while (incorrect_answer == correct_answer) {
+                        incorrect_answer = Random.Range(-2 * correct_answer, 2 * correct_answer);
+                    }
+                }
+
+                problem = operand_one.ToString() + " - " + operand_two.ToString() + " = ?";
+                break;
+            case 2:
+                // Multiplication
+                correct_answer = operand_one * operand_two;
+                while (incorrect_answer == correct_answer) {
+                    incorrect_answer = Random.Range(0, 2 * correct_answer);
+                }
+
+                problem = operand_one.ToString() + " * " + operand_two.ToString() + " = ?";
+                break;
+            case 3:
+                // Division
+                correct_answer = operand_one / operand_two;
+                while (incorrect_answer == correct_answer) {
+                    incorrect_answer = Random.Range(0, 2 * correct_answer);
+                }
+
+                problem = operand_one.ToString() + " / " + operand_two.ToString() + " = ?";
+                break;   
+        }
+        
+        prob_and_sols.Add("problem", problem);
+        prob_and_sols.Add("correct_answer", correct_answer.ToString());
+        prob_and_sols.Add("incorrect_answer", incorrect_answer.ToString());
+
+        return prob_and_sols;
+    }
+
+    Dictionary<string, string> GenerateProblem() {        
+
+        switch (hardness) {
+            case 1:
+                return GenProbWithRangeAndOperator(21, 0);
+            case 2:
+                return GenProbWithRangeAndOperator(21, 1);
+            case 3:
+                return GenProbWithRangeAndOperator(21, 2);
+            case 4: 
+                return GenProbWithRangeAndOperator(21, 3);
+            case 5:
+                return GenProbWithRangeAndOperator(21, Random.Range(0, 3));
+            default:
+                return GenProbWithRangeAndOperator(0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name.Contains("plane")) {
+            Debug.Log("Plane Collision");
+
+            Dictionary<string, string> prob_and_sols = GenerateProblem();
+
+            Vector3 problem_bubble_pos = other.transform.position;
+            problem_bubble_pos.z += 10;
+            problem_bubble_pos.y += 1;
+
+            Vector3 correct_bubble_pos = problem_bubble_pos;
+            correct_bubble_pos.y -= 2;
+            Vector3 incorrect_bubble_pos = correct_bubble_pos;
+
+            int is_correct_bubble_on_right = Random.Range(0, 2);
+            if (is_correct_bubble_on_right == 1) {
+                correct_bubble_pos.x += 2;
+                incorrect_bubble_pos.x -= 2;
+            }
+            else {
+                correct_bubble_pos.x -= 2;
+                incorrect_bubble_pos.x += 2;
+            }
+            
+            GameObject problem_bubble = Instantiate(bubble, problem_bubble_pos, Quaternion.identity);
+            problem_bubble.name = "PROBLEM_BUBBLE";
+            problem_bubble.GetComponent<BubbleHandler>().SetType(0);
+            problem_bubble.GetComponent<BubbleHandler>().SetText(prob_and_sols["problem"]);
+
+            GameObject correct_bubble = Instantiate(bubble, correct_bubble_pos, Quaternion.identity);
+            correct_bubble.name = "CORRECT_BUBBLE";
+            correct_bubble.GetComponent<BubbleHandler>().SetType(1);
+            correct_bubble.GetComponent<BubbleHandler>().SetText(prob_and_sols["correct_answer"]);
+
+            GameObject incorrect_bubble = Instantiate(bubble, incorrect_bubble_pos, Quaternion.identity);
+            incorrect_bubble.name = "INCORRECT_BUBBLE";
+            incorrect_bubble.GetComponent<BubbleHandler>().SetType(2);
+            incorrect_bubble.GetComponent<BubbleHandler>().SetText(prob_and_sols["incorrect_answer"]);
+
+            
+        }
+    }
+}
