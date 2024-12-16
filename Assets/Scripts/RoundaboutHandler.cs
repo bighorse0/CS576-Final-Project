a782ffb9c93ca798f50724425d9b3b961e056379
wylyK <willychen720360@gmail.com>
2024-12-16 16:41:22 -0500

    add scene 4

---
+++ Assets/Scripts/RoundaboutHandler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RoundaboutHandler : MonoBehaviour
+{
+    public GameObject center_pole;
+    private Vector3 center;
+    private Rigidbody RB;
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+        RB.mass = 1.0f;
+        RB.velocity = new Vector3(0.0f, 0.0f, 35.0f);
+        center = center_pole.transform.position;
+        center.y = 0.0f;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 position = transform.position;
+        position.y = 0.0f;
+        transform.position = position;
+        Vector3 radius_vector = center - position;
+        RB.AddForce(1225.0f / radius_vector.magnitude * radius_vector.normalized);
+        RB.AddForce(new Vector3(0.0f, 2.0f, 0.0f));
+        AlignNoseWithVelocity();
+    }
+
+    private void AlignNoseWithVelocity()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(RB.velocity.normalized, transform.up);
+        targetRotation = Quaternion.Euler(targetRotation.eulerAngles.x, targetRotation.eulerAngles.y, transform.eulerAngles.z);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
+    }
+}
